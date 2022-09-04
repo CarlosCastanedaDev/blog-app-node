42d79e62867b45e6c4216d52c9ccc4e9d1364621
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2022-09-04 11:07:43 -0500

    finish chapter 4

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = new express()
+const ejs = require('ejs')
 
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.listen(4000, () => {
@@ -9,14 +11,18 @@ app.listen(4000, () => {
 })
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
 })
 app.get('/about', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 })
 app.get('/contact', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 app.get('/post', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 })
