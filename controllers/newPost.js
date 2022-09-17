9a9ed08685750398d1c6b663f1a74655a903a28b
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2022-09-17 12:46:42 -0500

    finsih chapter 14

--- controllers/newPost.js
+++ controllers/newPost.js
@@ -1,6 +1,8 @@
 module.exports = (req,res) => {
     if(req.session.userId){
-    return res.render('create2')
+    return res.render('create2', {
+        createPost: true
+    })
     }
     res.redirect('/auth/login')
 }
